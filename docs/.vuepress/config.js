5acdaf55e947ca9fcb175e5739e96eaa6c712eb3
EngrZhou <engrzhou@gmail.com>
2020-04-24 14:39:55 +0800

    1. update config；

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -29,7 +29,7 @@ module.exports = {
     }],
     'vuepress-plugin-baidu-autopush',
     ['sitemap', {
-      hostname: 'https://moyou.fun'
+      hostname: 'https://moyou.club'
     }],
   ],
   themeConfig: {
